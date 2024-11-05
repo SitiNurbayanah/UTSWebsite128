17e36bd68a418cfa2536f0b965520d73c6aeb65f
IBayyy <119025967+SitiNurbayanah@users.noreply.github.com>
2024-11-05 16:30:20 +0700

    Add files via upload

---
+++ question_lists.js
@@ -0,0 +1,21 @@
+var quiz = [
+  // Pertanyaan Mendatar
+  [1, 2, "Alat untuk menulis dengan tinta", "PENA", "datar"],
+  [3, 2, "... pauk", "LAUK", "datar"],
+  [
+    5,
+    5,
+    "Alat komunikasi teknologi yang digunakan untuk pengiriman sinyal dengan cara modulasi dan radiasi elektromagnetik",
+    "RADIO",
+    "datar",
+  ],
+  [6, 1, "Apa yang biasanya ada di hari ulang tahun", "KUE", "datar"],
+  [8, 0, "Pasangan dari kursi", "MEJA", "datar"],
+
+  // Pertanyaan Menurun
+  [0, 2, "Buah berwarna merah dengan bentuk bulat", "APEL", "turun"],
+  [1, 8, "Makanan jepang dengan menggunakan ikan mentah", "SUSHI", "turun"],
+  [3, 3, "Penangkap sinyal televisi", "ANTENA", "turun"],
+  [3, 5, "Kotak yang dipakai untuk packing", "KARDUS", "turun"],
+  [5, 7, "Cara kita berbicara dengan tuhan", "DOA", "turun"],
+];
